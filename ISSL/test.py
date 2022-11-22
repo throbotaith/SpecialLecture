a1303eeff0909dc4be7d388a015e059ceb29e0c8
Teruoka Hajime <hajime-t@ws21is101.naist.jp>
2022-11-22 16:02:35 +0900

    Oh yeah

--- ISSL/test.py
+++ ISSL/test.py
@@ -1,22 +1,22 @@
 import unittest
-from CSVPrinter import CSVPrinter
+import CSVPrinter
 
 
 class TestCSVPrinter(unittest.TestCase):
 
     def test_read(self):
-        printer = CSVPrinter("sample.csv")
+        printer = CSVPrinter.CSVPrinter("sample.csv")
         l = printer.read()
         self.assertEqual(3, len(l))
 
     def test_read2(self):
-        printer = CSVPrinter("sample2.csv")
+        printer = CSVPrinter.CSVPrinter("sample.csv")
         line = printer.read()
         self.assertEqual("value2B", line[1][2])
 
     def test_read3(self):
         try:
-            printer = CSVPrinter("aaaa.csv")
+            printer = CSVPrinter.CSVPrinter("csv.csv")
             printer.read()
             unittest.TestCase.fail("This line should not be involked")
         except FileExistsError as error:
